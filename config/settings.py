e3455c05dc839c1e73c205dc8a4e9d3e3eb4909b
platinum-otabek <mr.tursinboev@gmail.com>
2022-01-14 00:37:54 +0500

    added ci

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
- 
+  
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
     from .local_settings import *
